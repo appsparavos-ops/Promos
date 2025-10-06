a5db4b25628ab04c05d188544f4fbfd242bbf60a
appsparavos-ops <appsparavos@gmail.com>
2025-10-06 17:42:12 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDBExMYiIefz4OXCZ7pLF6vy0sfZp7ergo",
+  authDomain: "azburguer-c512b.firebaseapp.com",
+  databaseURL: "https://azburguer-c512b-default-rtdb.firebaseio.com",
+  projectId: "azburguer-c512b",
+  storageBucket: "azburguer-c512b.firebasestorage.app",
+  messagingSenderId: "444971647817",
+  appId: "1:444971647817:web:8ee7528ab7559c71b1d098",
+  measurementId: "G-YQFPY123JR"
+};
